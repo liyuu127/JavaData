c351a73f838c9c3ce719f39869f9f1d1579de6ef
liyu <1170667320@qq.com>
2019-11-26 11:10:34 +0800

    CountDownLatch倒计时器和CyclicBarrier循环栅栏

---
+++ src/base/multithread/lock/CyclicBarrierExample2.java
@@ -0,0 +1,52 @@
+package base.multithread.lock;
+
+import java.util.concurrent.*;
+
+/**
+ * @author liyu
+ * @date 2019/11/26 10:30
+ * @description CyclicBarrier(循环栅栏)
+ * CyclicBarrier 和 CountDownLatch 非常类似，它也可以实现线程间的技术等待，但是它的功能比 CountDownLatch 更加复杂和强大。主要应用场景和 CountDownLatch 类似。
+ * CyclicBarrier 的字面意思是可循环使用（Cyclic）的屏障（Barrier）。它要做的事情是，让一组线程到达一个屏障（也可以叫同步点）时被阻塞，直到最后一个线程到达屏障时，屏障才会开门，所有被屏障拦截的线程才会继续干活。CyclicBarrier默认的构造方法是 CyclicBarrier(int parties)，其参数表示屏障拦截的线程数量，每个线程调用await方法告诉 CyclicBarrier 我已经到达了屏障，然后当前线程被阻塞。
+ * CyclicBarrier 可以用于多线程计算数据，最后合并计算结果的应用场景。比如我们用一个Excel保存了用户所有银行流水，每个Sheet保存一个帐户近一年的每笔银行流水，现在需要统计用户的日均银行流水，先用多线程处理每个sheet里的银行流水，都执行完之后，得到每个sheet的日均银行流水，最后，再用barrierAction用这些线程的计算结果，计算出整个Excel的日均银行流水。
+ */
+public class CyclicBarrierExample2 {
+    // 请求的数量
+    private static final int threadCount = 550;
+    // 需要同步的线程数量
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            threadPool.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+
+    public static void test(int threadnum) throws InterruptedException, BrokenBarrierException {
+        System.out.println("threadnum:" + threadnum + "is ready");
+        try {
+            /**等待60秒，保证子线程完全执行结束*/
+            cyclicBarrier.await(60, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println("-----CyclicBarrierException------");
+        }
+        System.out.println("threadnum:" + threadnum + "is finish");
+    }
+
+}
---
+++ src/base/multithread/lock/SemaphoreExample1.java
@@ -0,0 +1,46 @@
+package base.multithread.lock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author liyu
+ * @date 2019/11/26 9:26
+ * @description  Semaphore(信号量)-允许多个线程同时访问
+ * synchronized 和 ReentrantLock 都是一次只允许一个线程访问某个资源，Semaphore(信号量)可以指定多个线程同时访问某个资源
+ */
+public class SemaphoreExample1 {
+    // 请求的数量
+    private static final int threadCount = 550;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建一个具有固定线程数量的线程池对象（如果这里线程池的线程数量给太少的话你会发现执行的很慢）
+        ExecutorService threadPool = Executors.newFixedThreadPool(300);
+        // 一次只能允许执行的线程数量。
+        final Semaphore semaphore = new Semaphore(20,true);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadnum = i;
+            threadPool.execute(() -> {// Lambda 表达式的运用
+                try {
+                    semaphore.acquire();// 获取一个许可，所以可运行线程数量为20/1=20
+                    test(threadnum);
+                    semaphore.release();// 释放一个许可
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            });
+        }
+        threadPool.shutdown();
+        System.out.println("finish");
+    }
+
+    public static void test(int threadnum) throws InterruptedException {
+        Thread.sleep(1000);// 模拟请求的耗时操作
+        System.out.println("threadnum:" + threadnum);
+        Thread.sleep(1000);// 模拟请求的耗时操作
+    }
+}
