3111b1586fc6a21fff65775cda13ddad45907ecf
liyu <1170667320@qq.com>
2020-04-16 15:36:29 +0800

    两数相加II

---
+++ src/leetCode/array/general/AddTwoNumbersII.java
@@ -0,0 +1,63 @@
+package leetCode.array.general;
+
+import java.util.Stack;
+
+/**
+ * @author liyu
+ * @date 2020/4/14 14:09
+ * @description 两数相加II
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * 进阶：
+ * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+ * 示例：
+ * 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 8 -> 0 -> 7
+ */
+
+public class AddTwoNumbersII {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        //进位
+        int c = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || c > 0) {
+            int sum = c;
+            sum += stack1.isEmpty() ? 0 : stack1.pop();
+            sum += stack2.isEmpty() ? 0 : stack2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            c = sum / 10;
+        }
+
+        return head;
+    }
+
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
