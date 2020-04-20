b7d26316e4dff71433af0a0a2d54332d8a184ce1
liyu <1170667320@qq.com>
2020-04-20 14:04:44 +0800

    岛屿数量

---
+++ src/leetCode/fs/gengral/NumberOfIslands.java
@@ -0,0 +1,205 @@
+package leetCode.fs.gengral;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author liyu
+ * @date 2020/4/20 11:52
+ * @description 岛屿数量
+ * 给你一个由?'1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 示例 1:
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 输出:?1
+ * <p>
+ * 示例?2:
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@UtilityClass
+public class NumberOfIslands {
+
+    public static void main(String[] args) {
+        char[][] chars = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
+        int i = numIslands(chars);
+        System.out.println("i = " + i);
+    }
+
+    public int numIslands(char[][] grid) {
+        int r = grid.length;
+        if (r == 0) {
+            return 0;
+        }
+        int c = grid[0].length;
+        int n = 0;
+        int[][] dr = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1') {
+                    queue.offer(new int[]{i, j});
+                    n++;
+                    while (!queue.isEmpty()) {
+                        int[] poll = queue.poll();
+                        for (int[] ints : dr) {
+                            int row = poll[0] + ints[0];
+                            int clo = poll[1] + ints[1];
+                            if (row >= 0 && row < r && clo >= 0 && clo < c && grid[row][clo] == '1') {
+                                queue.offer(new int[]{row, clo});
+                                grid[row][clo] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return n;
+    }
+
+    public int numIslandsBFS(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    /**
+     * 为了求出岛屿的数量，我们可以扫描整个二维网格。如果一个位置为 11，则将其与相邻四个方向上的 11 在并查集中进行合并。
+     * <p>
+     * 最终岛屿的数量就是并查集中连通分量的数目
+     */
+    class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) {
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) {
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                        uf.union(r * nc + c, (r - 1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r + 1][c] == '1') {
+                        uf.union(r * nc + c, (r + 1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c + 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+
+}
