bc0fac0bca8cbd8add6bd3f1d40857fa8142913c
liyu <1170667320@qq.com>
2020-04-22 10:40:54 +0800

    二叉树的右视图

---
+++ src/leetCode/tree/BinaryTreeRightSideView.java
@@ -0,0 +1,138 @@
+package leetCode.tree;
+
+import java.util.*;
+
+/**
+ * @author liyu
+ * @date 2020/4/22 9:15
+ * @description 二叉树的右视图
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * 示例:
+ * 输入:?[1,2,3,null,5,null,4]
+ * 输出:?[1, 3, 4]
+ * 解释:
+ * 1            <---
+ * /   \
+ * 2     3         <---
+ * \     \
+ * 5     4       <---
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BinaryTreeRightSideView {
+
+    /**
+     * dfs
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<>();
+        int max_depth = -1;
+
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> depthStack = new Stack<>();
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int depth = depthStack.pop();
+
+            if (node != null) {
+                // 维护二叉树的最大深度
+                max_depth = Math.max(max_depth, depth);
+
+                // 如果不存在对应深度的节点我们才插入
+                if (!rightmostValueAtDepth.containsKey(depth)) {
+                    rightmostValueAtDepth.put(depth, node.val);
+                }
+
+                nodeStack.push(node.left);
+                nodeStack.push(node.right);
+                depthStack.push(depth + 1);
+                depthStack.push(depth + 1);
+            }
+        }
+
+        List<Integer> rightView = new ArrayList<>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+
+        return rightView;
+    }
+
+    /**
+     * dfs
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> rightView = new ArrayList<>();
+        dfs(rightView, root, 0);
+        return rightView;
+    }
+
+    public void dfs(List<Integer> rightView, TreeNode root, int dep) {
+        if (root != null) {
+            if (dep == rightView.size()) {
+                rightView.add(root.val);
+            }
+            dfs(rightView, root.right, dep + 1);
+            dfs(rightView, root.left, dep + 1);
+        }
+    }
+
+    /**
+     * bfs
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView3(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<Integer, Integer>();
+        int max_depth = -1;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<Integer> depthQueue = new LinkedList<Integer>();
+        nodeQueue.add(root);
+        depthQueue.add(0);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+            int depth = depthQueue.remove();
+
+            if (node != null) {
+                // 维护二叉树的最大深度
+                max_depth = Math.max(max_depth, depth);
+
+                // 由于每一层最后一个访问到的节点才是我们要的答案，因此不断更新对应深度的信息即可
+                rightmostValueAtDepth.put(depth, node.val);
+
+                nodeQueue.add(node.left);
+                nodeQueue.add(node.right);
+                depthQueue.add(depth+1);
+                depthQueue.add(depth+1);
+            }
+        }
+
+        List<Integer> rightView = new ArrayList<Integer>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+
+        return rightView;
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
