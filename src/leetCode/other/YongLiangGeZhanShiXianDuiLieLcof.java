b1e52d77d4e2d2e1abd863395e9c7ba89923a68a
liyu <1170667320@qq.com>
2020-06-30 09:42:22 +0800

    用两个栈实现队列

---
+++ src/leetCode/other/YongLiangGeZhanShiXianDuiLieLcof.java
@@ -0,0 +1,74 @@
+package leetCode.other;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Stack;
+
+/**
+ * @author liyu
+ * @date 2020/6/30 9:08
+ * @description 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead?操作返回 -1 )
+ * <p>
+ * 示例 1：
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ * 1 <= values <= 10000
+ * 最多会对?appendTail、deleteHead 进行?10000?次调用
+ * <p>
+ * 来源：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+ */
+@UtilityClass
+public class YongLiangGeZhanShiXianDuiLieLcof {
+
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(3);
+        int i = cQueue.deleteHead();
+        System.out.println("i = " + i);
+        int j = cQueue.deleteHead();
+        System.out.println("j = " + j);
+    }
+
+    /**
+     * Your CQueue object will be instantiated and called as such:
+     * CQueue obj = new CQueue();
+     * obj.appendTail(value);
+     * int param_2 = obj.deleteHead();
+     */
+    class CQueue {
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack2.isEmpty() && stack1.isEmpty()) {
+                return -1;
+            } else if (!stack2.isEmpty()) {
+                return stack2.pop();
+            } else {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+                return stack2.pop();
+            }
+        }
+    }
+}
