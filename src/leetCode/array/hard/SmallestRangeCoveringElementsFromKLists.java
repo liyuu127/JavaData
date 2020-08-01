0fae464803787c93840cf18204a03c043383588e
SaitoAsuk-a <1170667320@qq.com>
2020-08-01 10:37:11 +0800

    最小区间

---
+++ src/leetCode/array/hard/SmallestRangeCoveringElementsFromKLists.java
@@ -0,0 +1,71 @@
+package leetCode.array.hard;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @author liyu
+ * date 2020/8/1 9:00
+ * description 最小区间
+ * 你有?k?个升序排列的整数数组。找到一个最小区间，使得?k?个列表中的每个列表至少有一个数包含在其中。
+ * 我们定义如果?b-a < d-c?或者在?b-a == d-c?时?a < c，则区间 [a,b] 比 [c,d] 小。
+ * 示例 1:
+ * 输入:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
+ * 输出: [20,24]
+ * 解释:
+ * 列表 1：[4, 10, 15, 24, 26]，24 在区间 [20,24] 中。
+ * 列表 2：[0, 9, 12, 20]，20 在区间 [20,24] 中。
+ * 列表 3：[5, 18, 22, 30]，22 在区间 [20,24] 中。
+ * 注意:
+ * 给定的列表可能包含重复元素，所以在这里升序表示 >= 。
+ * 1 <= k <= 3500
+ * -105 <= 元素的值?<= 105
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/smallest-range-covering-elements-from-k-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@UtilityClass
+public class SmallestRangeCoveringElementsFromKLists {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 堆
+     * @param nums
+     * @return
+     */
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int rangeLeft = 0, rangeRight = Integer.MAX_VALUE;
+        int minRange = rangeRight - rangeLeft;
+        int max = Integer.MIN_VALUE;
+        int size = nums.size();
+        int[] next = new int[size];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(index -> nums.get(index).get(next[index])));
+        for (int i = 0; i < size; i++) {
+            priorityQueue.offer(i);
+            max = Math.max(max, nums.get(i).get(0));
+        }
+        while (true) {
+            int minIndex = priorityQueue.poll();
+            int curRange = max - nums.get(minIndex).get(next[minIndex]);
+            if (curRange < minRange) {
+                minRange = curRange;
+                rangeLeft = nums.get(minIndex).get(next[minIndex]);
+                rangeRight = max;
+            }
+            next[minIndex]++;
+            if (next[minIndex] == nums.get(minIndex).size()) {
+                break;
+            }
+            priorityQueue.offer(minIndex);
+            max = Math.max(max, nums.get(minIndex).get(next[minIndex]));
+        }
+        return new int[]{rangeLeft, rangeRight};
+    }
+
+}
