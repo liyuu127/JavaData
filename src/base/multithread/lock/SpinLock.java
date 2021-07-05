89799cd3ee18f117a2319b9570c100564ea181a9
liyu <liyu@haylion.cn>
2021-07-05 16:17:51 +0800

    feature(other): 迪杰斯特拉算法

--- src/base/multithread/lock/SpinLock.java
+++ src/base/multithread/lock/SpinLock.java
@@ -6,18 +6,20 @@ import java.util.concurrent.atomic.AtomicReference;
  * @author liyu
  * @date 2019/11/25 13:37
  * @description 自旋锁
+ * TASLock TTASLock
  */
 public class SpinLock {
     private AtomicReference<Thread> cas = new AtomicReference<Thread>();
     public void lock() {
         Thread current = Thread.currentThread();
-        // 利用CAS
+        // 如果锁未被占用，则设置当前线程为锁的拥有者
         while (!cas.compareAndSet(null, current)) {
             // DO nothing
         }
     }
     public void unlock() {
         Thread current = Thread.currentThread();
+        // 只有锁的拥有者才能释放锁
         cas.compareAndSet(current, null);
     }
 }
