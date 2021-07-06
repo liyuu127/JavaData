1645571ba20a8b6280810e4083d198917335e8af
liyu <liyu@haylion.cn>
2021-07-06 16:52:47 +0800

    三数之和

---
+++ src/leetCode/array/general/ThreeSum.java
@@ -0,0 +1,113 @@
+package leetCode.array.general;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author liyu
+ * date 2021/7/6 10:16
+ * description 三数之和
+ * 给你一个包含 n 个整数的数组?nums，判断?nums?中是否存在三个元素 a，b，c ，使得?a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ * 示例 1：
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 示例 2：
+ * 输入：nums = []
+ * 输出：[]
+ * 示例 3：
+ * 输入：nums = [0]
+ * 输出：[]
+ * 提示：
+ * 0 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+
+        List<List<Integer>> lists = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        System.out.println("lists = " + lists);
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = a + nums[l] + nums[r];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(a);
+                    list.add(nums[l]);
+                    list.add(nums[r]);
+                    result.add(list);
+                    while (l < r) {
+                        if (nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        if (nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                    }
+
+                } else if (sum < 3) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return result;
+    }
+        public List<List<Integer>> threeSum0(int[] nums) {
+            int n = nums.length;
+            Arrays.sort(nums);
+            List<List<Integer>> ans = new ArrayList<List<Integer>>();
+            // 枚举 a
+            for (int first = 0; first < n; ++first) {
+                // 需要和上一次枚举的数不相同
+                if (first > 0 && nums[first] == nums[first - 1]) {
+                    continue;
+                }
+                // c 对应的指针初始指向数组的最右端
+                int third = n - 1;
+                int target = -nums[first];
+                // 枚举 b
+                for (int second = first + 1; second < n; ++second) {
+                    // 需要和上一次枚举的数不相同
+                    if (second > first + 1 && nums[second] == nums[second - 1]) {
+                        continue;
+                    }
+                    // 需要保证 b 的指针在 c 的指针的左侧
+                    while (second < third && nums[second] + nums[third] > target) {
+                        --third;
+                    }
+                    // 如果指针重合，随着 b 后续的增加
+                    // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                    if (second == third) {
+                        break;
+                    }
+                    if (nums[second] + nums[third] == target) {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(nums[first]);
+                        list.add(nums[second]);
+                        list.add(nums[third]);
+                        ans.add(list);
+                    }
+                }
+            }
+            return ans;
+        }
+
+}
