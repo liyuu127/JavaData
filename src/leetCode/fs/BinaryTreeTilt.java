190fa672c151e3ff8556152e7a843d4c4cfc6185
liyu <liyu@haylion.cn>
2021-11-18 10:41:23 +0800

    二叉树的坡度

---
+++ src/leetCode/fs/BinaryTreeTilt.java
@@ -0,0 +1,59 @@
+package leetCode.fs;
+
+/**
+ * @author liyu
+ * date 2021/11/18 9:51
+ * description 二叉树的坡度
+ * 给定一个二叉树，计算 整个树 的坡度 。
+ * 一个树的 节点的坡度 定义即为，该节点左子树的节点之和和右子树节点之和的 差的绝对值 。
+ * 如果没有左子树的话，左子树的节点之和为 0 ；没有右子树的话也是一样。空结点的坡度是 0 。
+ * 整个树 的坡度就是其所有节点的坡度之和。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-tilt
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BinaryTreeTilt {
+    int gradient = 0;
+
+    public int findTilt(TreeNode root) {
+
+        dfs(root);
+        return gradient;
+    }
+
+    /**
+     * 返回左右树的和
+     *
+     * @param node
+     * @return
+     */
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int sumLeft = dfs(node.left);
+        int sumRight = dfs(node.right);
+        gradient += Math.abs(sumLeft - sumRight);
+        return sumLeft + sumRight + node.val;
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
