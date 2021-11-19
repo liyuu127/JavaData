367089cba0b91f2320719c8f0a15e7ed7be59a4c
liyu <liyu@haylion.cn>
2021-11-19 10:17:29 +0800

    整数替换

---
+++ src/leetCode/other/IntegerReplacement.java
@@ -0,0 +1,41 @@
+package leetCode.other;
+
+/**
+ * @author liyu
+ * date 2021/11/19 9:13
+ * description 整数替换
+ * 给定一个正整数 n ，你可以做如下操作：
+ * 如果 n 是偶数，则用 n / 2替换 n 。
+ * 如果 n 是奇数，则可以用 n + 1或n - 1替换 n 。
+ * n 变为 1 所需的最小替换次数是多少？
+ * 输入：n = 8
+ * 输出：3
+ * 解释：8 -> 4 -> 2 -> 1
+ * 示例 2：
+ * 输入：n = 7
+ * 输出：4
+ * 解释：7 -> 8 -> 4 -> 2 -> 1
+ * 或 7 -> 6 -> 3 -> 2 -> 1
+ * 示例 3：
+ * 输入：n = 4
+ * 输出：2
+ * 提示：
+ * 1 <= n <= 231 - 1
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-replacement
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-replacement
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IntegerReplacement {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2);
+        }
+        return 2 + Math.min(integerReplacement(n / 2 + 1), integerReplacement(n / 2));
+    }
+}
