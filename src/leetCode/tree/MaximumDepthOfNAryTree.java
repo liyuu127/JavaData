491fdcae1e479134648ef1b48d1cc636b7d9cb67
liyu <1170667320@qq.com>
2021-11-22 21:14:25 +0800

    N叉树的最大深度

---
+++ src/leetCode/tree/MaximumDepthOfNAryTree.java
@@ -0,0 +1,70 @@
+package leetCode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * N叉树的最大深度
+ * 给定一个 N 叉树，找到其最大深度。
+ * 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+ * N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaximumDepthOfNAryTree {
+
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        List<Node> children = root.children;
+        int maxDepth = 0;
+        for (int i = 0; i < children.size(); i++) {
+            maxDepth = Math.max(maxDepth(children.get(i)), maxDepth);
+        }
+        return 1 + maxDepth;
+    }
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+    public int maxDepth2(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                Node node = queue.poll();
+                List<Node> children = node.children;
+                for (Node child : children) {
+                    queue.offer(child);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+}
