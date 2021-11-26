d2b5402bc9f57d91319d1ad4b7b59b0f17c85dfa
liyu <liyu@haylion.cn>
2021-11-26 09:33:39 +0800

    二叉搜索树中的搜索

---
+++ src/leetCode/tree/SearchInABinarySearchTree.java
@@ -0,0 +1,47 @@
+package leetCode.tree;
+
+/**
+ * @author liyu
+ * date 2021/11/26 9:19
+ * description 二叉搜索树中的搜索
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * <p>
+ * 例如，
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) return null;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return root;
+        }
+    }
+
+    //     Definition for a binary tree node.
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
