228291810f83c285c659c49c0b9f370e3a3f7008
liyu <liyu@haylion.cn>
2021-12-08 16:36:35 +0800

    三个无重叠子数组的最大和

---
+++ src/leetCode/array/hard/maximum_sum_of_3_non_overlapping_subarrays.java
@@ -0,0 +1,116 @@
+package leetCode.array.hard;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author liyu
+ * date 2021/12/8 9:40
+ * description 三个无重叠子数组的最大和
+ * 给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且 3 * k 项的和最大的子数组，并返回这三个子数组。
+ * 以下标的数组形式返回结果，数组中的每一项分别指示每个子数组的起始位置（下标从 0 开始）。如果有多个结果，返回字典序最小的一个。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,1,2,6,7,5,1], k = 2
+ * 输出：[0,3,5]
+ * 解释：子数组 [1, 2], [2, 6], [7, 5] 对应的起始下标为 [0, 3, 5]。
+ * 也可以取 [2, 1], 但是结果 [1, 3, 5] 在字典序上更大。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,1,2,1,2,1,2,1], k = 2
+ * 输出：[0,2,4]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 2 * 104
+ * 1 <= nums[i] < 216
+ * 1 <= k <= floor(nums.length / 3)
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-sum-of-3-non-overlapping-subarrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@UtilityClass
+public class maximum_sum_of_3_non_overlapping_subarrays {
+
+    public int[] maxSumOfOneSubarray(int[] nums, int k) {
+        int[] ans = new int[1];
+        int sum1 = 0, maxSum1 = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum1 += nums[i];
+            if (i >= k - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    ans[0] = i - k + 1;
+                }
+                sum1 -= nums[i - k + 1];
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int[] ints = maxSumOfTwoSubarrays(new int[]{1, 2, 1, 2, 2, 5, 1, 41, 5, 9, 2}, 3);
+        System.out.println("ints = " + ints);
+    }
+
+    public int[] maxSumOfTwoSubarrays(int[] nums, int k) {
+        int[] ans = new int[2];
+        int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
+        int sum2 = 0, maxSum12 = 0;
+        for (int i = k; i < nums.length; ++i) {
+            sum1 += nums[i - k];
+            sum2 += nums[i];
+            if (i >= k * 2 - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    maxSum1Idx = i - k * 2 + 1;
+                }
+                if (maxSum1 + sum2 > maxSum12) {
+                    maxSum12 = maxSum1 + sum2;
+                    ans[0] = maxSum1Idx;
+                    ans[1] = i - k + 1;
+                }
+                sum1 -= nums[i - k * 2 + 1];
+                sum2 -= nums[i - k + 1];
+            }
+        }
+        return ans;
+    }
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int[] ans = new int[3];
+        int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
+        int sum2 = 0, maxSum12 = 0, maxSum12Idx1 = 0, maxSum12Idx2 = 0;
+        int sum3 = 0, maxTotal = 0;
+        for (int i = k * 2; i < nums.length; ++i) {
+            sum1 += nums[i - k * 2];
+            sum2 += nums[i - k];
+            sum3 += nums[i];
+            if (i >= k * 3 - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    maxSum1Idx = i - k * 3 + 1;
+                }
+                if (maxSum1 + sum2 > maxSum12) {
+                    maxSum12 = maxSum1 + sum2;
+                    maxSum12Idx1 = maxSum1Idx;
+                    maxSum12Idx2 = i - k * 2 + 1;
+                }
+                if (maxSum12 + sum3 > maxTotal) {
+                    maxTotal = maxSum12 + sum3;
+                    ans[0] = maxSum12Idx1;
+                    ans[1] = maxSum12Idx2;
+                    ans[2] = i - k + 1;
+                }
+                sum1 -= nums[i - k * 3 + 1];
+                sum2 -= nums[i - k * 2 + 1];
+                sum3 -= nums[i - k + 1];
+            }
+        }
+        return ans;
+    }
+
+}
