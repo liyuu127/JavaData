b4c156eaf5d46026c1fc140ed9a5dcf004536e67
liyu <liyu@haylion.cn>
2021-12-09 10:18:43 +0800

    有效的井字游戏

---
+++ src/leetCode/other/valid_tic_tac_toe_state.java
@@ -0,0 +1,78 @@
+package leetCode.other;
+
+/**
+ * @author liyu
+ * date 2021/12/9 9:38
+ * description 有效的井字游戏
+ * 用字符串数组作为井字游戏的游戏板 board。当且仅当在井字游戏过程中，玩家有可能将字符放置成游戏板所显示的状态时，才返回 true。
+ * 该游戏板是一个 3 x 3 数组，由字符 " "，"X" 和 "O" 组成。字符 " " 代表一个空位。
+ * 以下是井字游戏的规则：
+ * 玩家轮流将字符放入空位（" "）中。
+ * 第一个玩家总是放字符 “X”，且第二个玩家总是放字符 “O”。
+ * “X” 和 “O” 只允许放置在空位中，不允许对已放有字符的位置进行填充。
+ * 当有 3 个相同（且非空）的字符填充任何行、列或对角线时，游戏结束。
+ * 当所有位置非空时，也算为游戏结束。
+ * 如果游戏结束，玩家不允许再放置字符。
+ * 示例 1:
+ * 输入: board = ["O  ", "   ", "   "]
+ * 输出: false
+ * 解释: 第一个玩家总是放置“X”。
+ * 示例 2:
+ * 输入: board = ["XOX", " X ", "   "]
+ * 输出: false
+ * 解释: 玩家应该是轮流放置的。
+ * 示例 3:
+ * 输入: board = ["XXX", "   ", "OOO"]
+ * 输出: false
+ * 示例 4:
+ * 输入: board = ["XOX", "O O", "XOX"]
+ * 输出: true
+ * 说明:
+ * 游戏板 board 是长度为 3 的字符串数组，其中每个字符串 board[i] 的长度为 3。
+ *  board[i][j] 是集合 {" ", "X", "O"} 中的一个字符。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-tic-tac-toe-state
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class valid_tic_tac_toe_state {
+
+    public boolean validTicTacToe(String[] board) {
+        int xCount = 0, oCount = 0;
+        for (String row : board) {
+            for (char c : row.toCharArray()) {
+                xCount = (c == 'X') ? (xCount + 1) : xCount;
+                oCount = (c == 'O') ? (oCount + 1) : oCount;
+            }
+        }
+        if (oCount != xCount && oCount != xCount - 1) {
+            return false;
+        }
+        if (win(board, 'X') && oCount != xCount - 1) {
+            return false;
+        }
+        if (win(board, 'O') && oCount != xCount) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean win(String[] board, char p) {
+        for (int i = 0; i < 3; ++i) {
+            if (p == board[0].charAt(i) && p == board[1].charAt(i) && p == board[2].charAt(i)) {
+                return true;
+            }
+            if (p == board[i].charAt(0) && p == board[i].charAt(1) && p == board[i].charAt(2)) {
+                return true;
+            }
+        }
+        if (p == board[0].charAt(0) && p == board[1].charAt(1) && p == board[2].charAt(2)) {
+            return true;
+        }
+        if (p == board[0].charAt(2) && p == board[1].charAt(1) && p == board[2].charAt(0)) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
