6580499bd60fb11b1749b7e8191e2d1704ff7a36
liyu <1170667320@qq.com>
2022-04-05 17:27:43 +0800

    线段树

---
+++ src/dataStructure/collection/tree/SegmentTree.java
@@ -0,0 +1,48 @@
+package dataStructure.collection.tree;
+
+public class SegmentTree {
+    private int[] segmentTree;
+
+    private SegmentTree(int n) {
+        segmentTree = new int[n];
+    }
+
+    private void build(int node, int s, int e, int[] nums) {
+        if (s == e) {
+            segmentTree[node] = nums[s];
+            return;
+        }
+        int m = s + (e - s) / 2;
+        build(node * 2 + 1, s, m, nums);
+        build(node * 2 + 2, m + 1, e, nums);
+        segmentTree[node] = segmentTree[node * 2 + 1] + segmentTree[node * 2 + 2];
+    }
+
+    private void change(int index, int val, int node, int s, int e) {
+        if (s == e) {
+            segmentTree[node] = val;
+            return;
+        }
+        int m = s + (e - s) / 2;
+        if (index <= m) {
+            change(index, val, node * 2 + 1, s, m);
+        } else {
+            change(index, val, node * 2 + 2, m + 1, e);
+        }
+        segmentTree[node] = segmentTree[node * 2 + 1] + segmentTree[node * 2 + 2];
+    }
+
+    private int range(int left, int right, int node, int s, int e) {
+        if (left == s && right == e) {
+            return segmentTree[node];
+        }
+        int m = s + (e - s) / 2;
+        if (right <= m) {
+            return range(left, right, node * 2 + 1, s, m);
+        } else if (left > m) {
+            return range(left, right, node * 2 + 2, m + 1, e);
+        } else {
+            return range(left, m, node * 2 + 1, s, m) + range(m + 1, right, node * 2 + 2, m + 1, e);
+        }
+    }
+}
