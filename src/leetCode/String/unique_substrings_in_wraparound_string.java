81945c74aea6d41290888d0af565c7349157cfd4
liyu <liyu@haylion.cn>
2022-05-25 10:27:29 +0800

    环绕字符串中唯一的子字符串

---
+++ src/leetCode/String/unique_substrings_in_wraparound_string.java
@@ -0,0 +1,53 @@
+package leetCode.String;
+
+import java.util.Arrays;
+
+/**
+ * @author liyu
+ * date 2022/5/25 9:16
+ * description 环绕字符串中唯一的子字符串
+ * 把字符串 s 看作是 “abcdefghijklmnopqrstuvwxyz” 的无限环绕字符串，所以 s 看起来是这样的：
+ * "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd...." . 
+ * 现在给定另一个字符串 p 。返回 s 中 唯一 的 p 的 非空子串 的数量 。 
+ * 示例 1:
+ * 输入: p = "a"
+ * 输出: 1
+ * 解释: 字符串 s 中只有一个"a"子字符。
+ * 示例 2:
+ * 输入: p = "cac"
+ * 输出: 2
+ * 解释: 字符串 s 中的字符串“cac”只有两个子串“a”、“c”。.
+ * 示例 3:
+ * 输入: p = "zab"
+ * 输出: 6
+ * 解释: 在字符串 s 中有六个子串“z”、“a”、“b”、“za”、“ab”、“zab”。
+ * 提示:
+ * 1 <= p.length <= 105
+ * p 由小写英文字母构成
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/unique-substrings-in-wraparound-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class unique_substrings_in_wraparound_string {
+    public static void main(String[] args) {
+        int cac = findSubstringInWraproundString("zab");
+        System.out.println("cac = " + cac);
+    }
+
+    public static int findSubstringInWraproundString(String p) {
+
+        int[] dp = new int[26];
+        int k = 0;
+        for (int i = 0; i < p.length(); ++i) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) + 26) % 26 == 1) { // 字符之差为 1 或 -25
+                ++k;
+            } else {
+                k = 1;
+            }
+            dp[p.charAt(i) - 'a'] = Math.max(dp[p.charAt(i) - 'a'], k);
+        }
+        return Arrays.stream(dp).sum();
+
+    }
+}
+
