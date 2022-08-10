52f8ef392a06f244a7013d892c2d7d8ca4853f01
liyu <liyu@haylion.cn>
2022-08-10 09:46:15 +0800

    特殊的二进制序列

---
+++ src/leetCode/String/special_binary_string.java
@@ -0,0 +1,63 @@
+package leetCode.String;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author liyu
+ * date 2022/8/8 15:40
+ * description 特殊的二进制序列
+ * 特殊的二进制序列是具有以下两个性质的二进制序列：
+ * 0 的数量与 1 的数量相等。
+ * 二进制序列的每一个前缀码中 1 的数量要大于等于 0 的数量。
+ * 给定一个特殊的二进制序列 S，以字符串形式表示。定义一个操作 为首先选择 S 的两个连续且非空的特殊的子串，然后将它们交换。
+ * （两个子串为连续的当且仅当第一个子串的最后一个字符恰好为第二个子串的第一个字符的前一个字符。)
+ * 在任意次数的操作之后，交换后的字符串按照字典序排列的最大的结果是什么？
+ * 示例 1:
+ * 输入: S = "11011000"
+ * 输出: "11100100"
+ * 解释:
+ * 将子串 "10" （在S[1]出现） 和 "1100" （在S[3]出现）进行交换。
+ * 这是在进行若干次操作后按字典序排列最大的结果。
+ * 说明:
+ * S 的长度不超过 50。
+ * S 保证为一个满足上述定义的特殊 的二进制序列。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/special-binary-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class special_binary_string {
+
+    public static void main(String[] args) {
+        String s = makeLargestSpecial("11011000");
+        System.out.println("s = " + s);
+    }
+
+    public static String makeLargestSpecial(String s) {
+        if (s.length() <= 2) {
+            return s;
+        }
+        int cnt = 0, left = 0;
+        List<String> subs = new ArrayList<String>();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '1') {
+                ++cnt;
+            } else {
+                --cnt;
+                if (cnt == 0) {
+                    subs.add("1" + makeLargestSpecial(s.substring(left + 1, i)) + "0");
+                    left = i + 1;
+                }
+            }
+        }
+
+        Collections.sort(subs, (a, b) -> b.compareTo(a));
+        StringBuilder ans = new StringBuilder();
+        for (String sub : subs) {
+            ans.append(sub);
+        }
+        return ans.toString();
+    }
+
+}
