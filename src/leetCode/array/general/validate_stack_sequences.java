d1229399731414a03181469d8e0838d7dab3caf5
liyu <liyu@haylion.cn>
2022-08-31 10:45:58 +0800

    验证栈序列

---
+++ src/leetCode/array/general/validate_stack_sequences.java
@@ -0,0 +1,56 @@
+package leetCode.array.general;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author liyu
+ * date 2022/8/31 9:49
+ * description 946. 验证栈序列
+ * 给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，
+ * 只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，
+ * 返回 true；否则，返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * 输出：true
+ * 解释：我们可以按以下顺序执行：
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * <p>
+ * 示例 2：
+ * 输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * 输出：false
+ * 解释：1 不能在 2 之前弹出。
+ * <p>
+ * 提示：
+ * 1 <= pushed.length <= 1000
+ * 0 <= pushed[i] <= 1000
+ * pushed 的所有元素 互不相同
+ * popped.length == pushed.length
+ * popped 是 pushed 的一个排列
+ */
+
+public class validate_stack_sequences {
+    public static void main(String[] args) {
+        boolean b = validateStackSequences(new int[]{0, 2, 1}, new int[]{0, 1, 2});
+        System.out.println("b = " + b);
+    }
+
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int n = pushed.length;
+        for (int i = 0, j = 0; i < n; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+
+
+    }
+
+
+}
